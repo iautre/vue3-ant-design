0378a6213c9e2d9190332c23f24ae3ad929705be
a little <mo@autre.cn>
2020-09-07 10:11:33 +0800

    更改图片地址

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
     <a :href="'https://autre.me'">Autre</a> &nbsp;
     <template v-if="beian">
     <a :href="'http://www.miitbeian.gov.cn/'" target="_blank">陕ICP备13008071号</a>&nbsp;
-    <img :src="'http://autre.cn/beian.png'" style="width: 16px; height: auto; max-width: 100%; vertical-align: middle; border: 0;"/>
+    <img :src="'https://autre.cn/beian.png'" style="width: 16px; height: auto; max-width: 100%; vertical-align: middle; border: 0;"/>
     <a :href="'http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=61012402000127'" target="_blank">陕公网安备61012402000127</a>&nbsp; 
     </template>
     <router-link to="/partner" >友友们</router-link>
